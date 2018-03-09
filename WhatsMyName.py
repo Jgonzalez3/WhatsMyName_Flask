b62aa52999e40ef43d6ca5892c3e7458f8f7a91d
Javier Gonzalez <javig56@gmail.com>
2018-03-08 19:18:01 -0800

    upload Flask Whats my name assignment

---
+++ WhatsMyName.py
@@ -0,0 +1,17 @@
+# pylint: disable=print-statement
+
+from flask import Flask, render_template, request, redirect
+app = Flask(__name__)
+@app.route("/")
+def index():
+    return render_template("WhatsMyName.html")
+
+@app.route("/process", methods=['POST'])
+def process():
+    print "Got Post Info"
+    name = request.form["name"]
+    email = request.form["email"]
+    print "Name:", name, "Email:", email
+
+    return redirect("/")
+app.run(debug=True)
